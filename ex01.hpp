47fe9cb5ec3c971517f06334dc2840ef3c335e2a
Arloste <88305350+Arloste@users.noreply.github.com>
2022-03-07 17:06:46 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -1,8 +1,5 @@
-//change the below code
-// the function below now only works with integers, change it to work with any type
-// also fill out the body
-template <typename WhatKind>
-void multiples (WhatKind& sum, WhatKind x, int n)
+template <typename T>
+void multiples (T& sum, T x, int n)
 {
     sum = 1 + x*(n+1)*n/2;
 }
