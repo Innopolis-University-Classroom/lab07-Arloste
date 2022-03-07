45c2c26bbef8122cf0d77b3ebb3a7b24afb24fd5
Arloste <88305350+Arloste@users.noreply.github.com>
2022-03-07 17:25:44 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -1,11 +1,5 @@
-
-
-// change the below code
-// the function below now only works with integers, change it to work with any
-// type also fill out the body
-template <typename T> void multiples(T &sum, T x, int n) {
-  sum = 1;
-  for (int multiple = 1; multiple <= n; multiple++) {
-    sum += multiple * x;
-  }
+template <typename T>
+void multiples(T& sum, T x, int n)
+{
+  sum = 1 + x*(n+1)*n/2;
 }
